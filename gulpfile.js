6bc69fb410759485ee88b6c2d5411eecbdd0cc8b
percevil <peeevil@gmail.com>
2019-10-21 13:45:15 -0700

    added eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,37 @@
-
-
 const gulp = require("gulp"),
     terser = require("gulp-terser"),
     rename = require("gulp-rename"),
-    browserSync = require("browser-sync");
+    browserSync = require("browser-sync"),
+    eslint = require("gulp-eslint");
 
-
-gulp.task("scripts", function () {
+gulp.task("lint", function () {
     return gulp
         .src("./js/*.js")
-        .pipe(terser())
-        .pipe(rename({
-            extname: ".min.js"
-        }))
-        .pipe(gulp.dest("./build/js"));
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
+
+
+
+gulp.task("scripts",
+    gulp.series("lint",
+        function scripts () {
+            return gulp
+                .src("./js/*.js")
+                .pipe(terser())
+                .pipe(rename({extname: ".min.js"}))
+                .pipe(gulp.dest("./build/js"));
+        }
+    )
+);
 
 gulp.task("say_hello", function (done) {
     console.log("Hello!");
     done();
 });
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browserSync.init({
         server: {
             baseDir: "./"
@@ -31,13 +41,13 @@ gulp.task('browser-sync', function() {
 
 
 
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', function () {
     browserSync.init({
         proxy: "yourlocal.dev"
     });
 
-    gulp.watch(["*.html","build/js/*.js", "css/*.css"])
-    .on("change", browserSync.reload);
+    gulp.watch(["*.html", "build/js/*.js", "css/*.css"])
+        .on("change", browserSync.reload);
 
 });
 
